12653bfd70e834555fc1af565a591e5a404a45d1
hamza5999 <mhamzamahmood5@gmail.com>
2023-10-04 14:03:09 +0500

    Separated Gradient logic for refatoring and reusability

---
+++ lib/gradient_container.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+const beginAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  final Color color1;
+  final Color color2;
+
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: beginAlignment,
+            end: endAlignment,
+            colors: [color1, color2]),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              "assets/images/quiz-logo.png",
+              width: 250,
+            ),
+            const SizedBox(height: 70),
+            const Text(
+              "Learn Flutter the fun way!",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/gradient_container.dart';
 
-const beginAlignment = Alignment.topLeft;
-const endAlignment = Alignment.bottomRight;
+
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: beginAlignment,
-                end: endAlignment,
-                colors: [Colors.amber, Colors.deepOrange]),
-          ),
-          child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  "assets/images/quiz-logo.png",
-                  width: 250,
-                ),
-                const SizedBox(height: 70),
-                const Text(
-                  "Learn Flutter the fun way!",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 25,
-                  ),
-                ),
-              ],
-            ),
-          ),
+        body: GradientContainer(
+          Color.fromARGB(255, 33, 5, 109),
+          Color.fromARGB(255, 68, 21, 149),
         ),
       ),
     ),
