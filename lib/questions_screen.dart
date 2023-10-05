354fd5631a25d550cb09e8e769633380fa761f75
hamza5999 <mhamzamahmood5@gmail.com>
2023-10-05 17:59:23 +0500

    Add dummy data and add a custom styled button widget for reusability

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -12,6 +13,25 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Questions Screen");
+    // Cenetred using SizedBox and mainAxisAlignment property of Column widget
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Question",
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          AnswerButton(answerText: "Answer 1", onTap: () {}),
+          AnswerButton(answerText: "Answer 2", onTap: () {}),
+          AnswerButton(answerText: "Answer 3", onTap: () {}),
+          AnswerButton(answerText: "Answer 4", onTap: () {}),
+        ],
+      ),
+    );
   }
 }
