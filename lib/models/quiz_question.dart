6e3db4a35122abab523625cc15ff8321613e85c3
hamza5999 <mhamzamahmood5@gmail.com>
2023-10-06 11:46:38 +0500

    Shuffle answer list elements using shuffle()

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,12 @@ class QuizQuestion {
   final List<String> answers;
 
   const QuizQuestion(this.text, this.answers);
+
+  List<String> getShuffledAnswers() {
+    final List<String> shuffledAnswers =
+        List.of(answers); // List.of copies a List
+    shuffledAnswers.shuffle();
+
+    return shuffledAnswers;
+  }
 }
